8954d9f5872afdf20bfe315be05b654625d2a380
trangnguyenpp <trangnguyenpp@gmail.com>
2022-11-07 21:06:29 +0800

    upload disneyplus_analysis jupyter notebook

--- disneyPlus_analysis.py
+++ disneyPlus_analysis.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+disney_path = "Data/disney_plus_titles.csv"
+disney_file = pd.read_csv(disney_path)
+
+
+# reduced_file = disney_file.dropna(how='any')
+reduced_file = disney_file.drop_duplicates(subset="title")
+
+# check type
+type_gb = reduced_file.groupby("type").count()
